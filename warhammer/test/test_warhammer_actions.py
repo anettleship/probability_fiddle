3510c0820b1974996ab6a06e22d9ebdc5d703d24
Adrian Nettleship <a.nettleship@gmail.com>
2025-11-24 10:48:29 +0000

    add ranged and melee hit probability

--- warhammer/test/test_warhammer_actions.py
+++ warhammer/test/test_warhammer_actions.py
@@ -21,6 +21,20 @@ def test_melee_attack_action_properties():
     assert attack_action.target == target, "Target should be the unit that is being attacked"
     assert attack_action.weapon == melee_weapon, "Weapon should be the weapon used in the attack"
 
-def test_ranged_attack_hit_probability():
+def test_ranged_attack_hit_probability_should_be_correct_for_space_marine():
 
-    pass
+    attacker = Unit(name="Space Marine", movement=6, toughness=4, save=3, wounds=2, leadership=8, objective_control=1)
+    target = Unit(name="Necron Warrior", movement=5, toughness=4, save=4, wounds=1, leadership=7, objective_control=2)
+    ranged_weapon = RangedWeapon(name="Bolter", range=24, attacks=1, ballistic_skill=4, strength=4, armour_penetration=-1, damage=1, bearer=attacker)
+    attack_action = RangedAttack(attacker=attacker, target=target, weapon=ranged_weapon) 
+    expected_hit_probability = 1 / 2  # 4+ to hit on a D6
+    assert attack_action.probability_to_hit() == expected_hit_probability, "Ranged attack hit probability should be correct based on ballistic skill"
+
+def test_melee_attack_hit_probability_should_be_correct_for_space_marine():
+
+    attacker = Unit(name="Space Marine", movement=6, toughness=4, save=3, wounds=2, leadership=8, objective_control=1)
+    target = Unit(name="Necron Warrior", movement=5, toughness=4, save=4, wounds=1, leadership=7, objective_control=2)
+    melee_weapon = MeleeWeapon(name="Chainsword", attacks=2, weapon_skill=3, strength=4, armour_penetration=-1, damage=1, bearer=attacker)
+    attack_action = MeleeAttack(attacker=attacker, target=target, weapon=melee_weapon) 
+    expected_hit_probability = 2 / 3  # 3+ to hit on a D6
+    assert attack_action.probability_to_hit() == expected_hit_probability, "Melee attack hit probability should be correct based on weapon skill"
--- warhammer/warhammer_actions.py
+++ warhammer/warhammer_actions.py
@@ -10,7 +10,17 @@ class RangedAttack(Attack):
         super().__init__(attacker, target)
         self.weapon = weapon 
 
+    def probability_to_hit(self):
+        required_roll = self.weapon.ballistic_skill
+        successful_outcomes = 7 - required_roll  # e.g., for 4+, successful outcomes are 4,5,6 => 3 outcomes
+        return successful_outcomes / 6
+
 class MeleeAttack(Attack):
     def __init__(self, attacker: Unit, target:Unit, weapon: MeleeWeapon):
         super().__init__(attacker, target)
         self.weapon = weapon
+
+    def probability_to_hit(self):
+        required_roll = self.weapon.weapon_skill
+        successful_outcomes = 7 - required_roll  # e.g., for 3+, successful outcomes are 3,4,5,6 => 4 outcomes
+        return successful_outcomes / 6
